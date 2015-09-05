922487426312c7d125a9d01458a253957ac08a22
Artiwarah Damrongchai <artiwarahd@gmail.com>
2015-09-06 00:21:12 +0700

    fine tune UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  scope "/:locale" do
+  scope "/:locale", :locale => /en|th/ do
     resources :expenses do
       member do 
         get :expense_details
