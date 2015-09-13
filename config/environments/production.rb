67971ab8d052f1778ae49f180243fc8ccea6f00a
Artiwarah Damrongchai <artiwarahd@gmail.com>
2015-09-13 23:31:25 +0700

    set notification from field

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.notifier_from_field = "do-not-reply@gmail.com"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
